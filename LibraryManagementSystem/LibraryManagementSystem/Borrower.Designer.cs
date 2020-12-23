e7d52fc7b5a01de03ec040a3d855ff8bc9896a06
Dilan Ranawaka <47558301+DilanRanawaka@users.noreply.github.com>
2020-12-23 12:42:11 +0530

    Create files

---
+++ LibraryManagementSystem/LibraryManagementSystem/Borrower.Designer.cs
@@ -0,0 +1,313 @@
+﻿
+namespace LibraryManagementSystem
+{
+    partial class Borrower
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btn_Clear = new System.Windows.Forms.Button();
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.btn_Update = new System.Windows.Forms.Button();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btn_Search = new System.Windows.Forms.Button();
+            this.btn_Copy = new System.Windows.Forms.Button();
+            this.btn_Borrower = new System.Windows.Forms.Button();
+            this.btn_Author = new System.Windows.Forms.Button();
+            this.btn_Book = new System.Windows.Forms.Button();
+            this.btn_Dashboard = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel1.Controls.Add(this.btn_Copy);
+            this.panel1.Controls.Add(this.btn_Borrower);
+            this.panel1.Controls.Add(this.btn_Author);
+            this.panel1.Controls.Add(this.btn_Book);
+            this.panel1.Controls.Add(this.btn_Dashboard);
+            this.panel1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Location = new System.Drawing.Point(1, 54);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(220, 728);
+            this.panel1.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(1, -1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1183, 55);
+            this.panel2.TabIndex = 4;
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Clear.Location = new System.Drawing.Point(726, 379);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(75, 23);
+            this.btn_Clear.TabIndex = 17;
+            this.btn_Clear.Text = "CLEAR";
+            this.btn_Clear.UseVisualStyleBackColor = true;
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Delete.Location = new System.Drawing.Point(617, 379);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(75, 23);
+            this.btn_Delete.TabIndex = 16;
+            this.btn_Delete.Text = "DELETE";
+            this.btn_Delete.UseVisualStyleBackColor = true;
+            // 
+            // btn_Update
+            // 
+            this.btn_Update.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Update.Location = new System.Drawing.Point(502, 379);
+            this.btn_Update.Name = "btn_Update";
+            this.btn_Update.Size = new System.Drawing.Size(75, 23);
+            this.btn_Update.TabIndex = 15;
+            this.btn_Update.Text = "UPDATE";
+            this.btn_Update.UseVisualStyleBackColor = true;
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Add.Location = new System.Drawing.Point(384, 379);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(75, 23);
+            this.btn_Add.TabIndex = 14;
+            this.btn_Add.Text = "ADD";
+            this.btn_Add.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(337, 229);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 16);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(337, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 16);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Borrower ID";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(486, 178);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(193, 20);
+            this.textBox1.TabIndex = 22;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(486, 229);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(193, 20);
+            this.textBox2.TabIndex = 23;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(464, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(293, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "LIBRARY MANAGEMENT SYSTEM";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(29, 21);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(221, 20);
+            this.textBox3.TabIndex = 24;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel3.Controls.Add(this.btn_Search);
+            this.panel3.Controls.Add(this.textBox3);
+            this.panel3.Location = new System.Drawing.Point(776, 178);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(377, 67);
+            this.panel3.TabIndex = 25;
+            // 
+            // btn_Search
+            // 
+            this.btn_Search.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Search.Location = new System.Drawing.Point(282, 19);
+            this.btn_Search.Name = "btn_Search";
+            this.btn_Search.Size = new System.Drawing.Size(75, 23);
+            this.btn_Search.TabIndex = 25;
+            this.btn_Search.Text = "Search";
+            this.btn_Search.UseVisualStyleBackColor = true;
+            // 
+            // btn_Copy
+            // 
+            this.btn_Copy.BackColor = System.Drawing.Color.SteelBlue;
+            this.btn_Copy.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Copy.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Copy.Location = new System.Drawing.Point(11, 454);
+            this.btn_Copy.Name = "btn_Copy";
+            this.btn_Copy.Size = new System.Drawing.Size(197, 39);
+            this.btn_Copy.TabIndex = 15;
+            this.btn_Copy.Text = "COPY";
+            this.btn_Copy.UseVisualStyleBackColor = false;
+            // 
+            // btn_Borrower
+            // 
+            this.btn_Borrower.BackColor = System.Drawing.Color.SteelBlue;
+            this.btn_Borrower.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Borrower.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Borrower.Location = new System.Drawing.Point(11, 367);
+            this.btn_Borrower.Name = "btn_Borrower";
+            this.btn_Borrower.Size = new System.Drawing.Size(197, 39);
+            this.btn_Borrower.TabIndex = 14;
+            this.btn_Borrower.Text = "BORROWER";
+            this.btn_Borrower.UseVisualStyleBackColor = false;
+            // 
+            // btn_Author
+            // 
+            this.btn_Author.BackColor = System.Drawing.Color.SteelBlue;
+            this.btn_Author.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Author.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Author.Location = new System.Drawing.Point(11, 280);
+            this.btn_Author.Name = "btn_Author";
+            this.btn_Author.Size = new System.Drawing.Size(197, 39);
+            this.btn_Author.TabIndex = 13;
+            this.btn_Author.Text = "AUTHOR";
+            this.btn_Author.UseVisualStyleBackColor = false;
+            // 
+            // btn_Book
+            // 
+            this.btn_Book.BackColor = System.Drawing.Color.SteelBlue;
+            this.btn_Book.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Book.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Book.Location = new System.Drawing.Point(11, 191);
+            this.btn_Book.Name = "btn_Book";
+            this.btn_Book.Size = new System.Drawing.Size(197, 39);
+            this.btn_Book.TabIndex = 12;
+            this.btn_Book.Text = "BOOK";
+            this.btn_Book.UseVisualStyleBackColor = false;
+            // 
+            // btn_Dashboard
+            // 
+            this.btn_Dashboard.BackColor = System.Drawing.Color.SteelBlue;
+            this.btn_Dashboard.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Dashboard.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Dashboard.Location = new System.Drawing.Point(11, 105);
+            this.btn_Dashboard.Name = "btn_Dashboard";
+            this.btn_Dashboard.Size = new System.Drawing.Size(197, 39);
+            this.btn_Dashboard.TabIndex = 11;
+            this.btn_Dashboard.Text = "DASHBOARD";
+            this.btn_Dashboard.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(340, 543);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(762, 176);
+            this.dataGridView1.TabIndex = 28;
+            // 
+            // Borrower
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 781);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Clear);
+            this.Controls.Add(this.btn_Delete);
+            this.Controls.Add(this.btn_Update);
+            this.Controls.Add(this.btn_Add);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "Borrower";
+            this.Text = "Borrower";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btn_Clear;
+        private System.Windows.Forms.Button btn_Delete;
+        private System.Windows.Forms.Button btn_Update;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btn_Search;
+        private System.Windows.Forms.Button btn_Copy;
+        private System.Windows.Forms.Button btn_Borrower;
+        private System.Windows.Forms.Button btn_Author;
+        private System.Windows.Forms.Button btn_Book;
+        private System.Windows.Forms.Button btn_Dashboard;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ LibraryManagementSystem/LibraryManagementSystem/Home.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace LibraryManagementSystem
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_Borrower = new System.Windows.Forms.Button();
+            this.btn_Author = new System.Windows.Forms.Button();
+            this.btn_Book = new System.Windows.Forms.Button();
+            this.btn_Dashboard = new System.Windows.Forms.Button();
+            this.btn_Logout = new System.Windows.Forms.Button();
+            this.btn_Copy = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel1.Controls.Add(this.btn_Copy);
+            this.panel1.Controls.Add(this.btn_Borrower);
+            this.panel1.Controls.Add(this.btn_Author);
+            this.panel1.Controls.Add(this.btn_Book);
+            this.panel1.Controls.Add(this.btn_Dashboard);
+            this.panel1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Location = new System.Drawing.Point(1, 53);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(220, 728);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel2.Controls.Add(this.btn_Logout);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(1, 1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1183, 55);
+            this.panel2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(445, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(293, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "LIBRARY MANAGEMENT SYSTEM";
+            // 
+            // btn_Borrower
+            // 
+            this.btn_Borrower.BackColor = System.Drawing.Color.SteelBlue;
+            this.btn_Borrower.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Borrower.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Borrower.Location = new System.Drawing.Point(11, 336);
+            this.btn_Borrower.Name = "btn_Borrower";
+            this.btn_Borrower.Size = new System.Drawing.Size(197, 39);
+            this.btn_Borrower.TabIndex = 9;
+            this.btn_Borrower.Text = "BORROWER";
+            this.btn_Borrower.UseVisualStyleBackColor = false;
+            // 
+            // btn_Author
+            // 
+            this.btn_Author.BackColor = System.Drawing.Color.SteelBlue;
+            this.btn_Author.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Author.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Author.Location = new System.Drawing.Point(11, 249);
+            this.btn_Author.Name = "btn_Author";
+            this.btn_Author.Size = new System.Drawing.Size(197, 39);
+            this.btn_Author.TabIndex = 8;
+            this.btn_Author.Text = "AUTHOR";
+            this.btn_Author.UseVisualStyleBackColor = false;
+            // 
+            // btn_Book
+            // 
+            this.btn_Book.BackColor = System.Drawing.Color.SteelBlue;
+            this.btn_Book.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Book.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Book.Location = new System.Drawing.Point(11, 160);
+            this.btn_Book.Name = "btn_Book";
+            this.btn_Book.Size = new System.Drawing.Size(197, 39);
+            this.btn_Book.TabIndex = 7;
+            this.btn_Book.Text = "BOOK";
+            this.btn_Book.UseVisualStyleBackColor = false;
+            // 
+            // btn_Dashboard
+            // 
+            this.btn_Dashboard.BackColor = System.Drawing.Color.SteelBlue;
+            this.btn_Dashboard.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Dashboard.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Dashboard.Location = new System.Drawing.Point(11, 74);
+            this.btn_Dashboard.Name = "btn_Dashboard";
+            this.btn_Dashboard.Size = new System.Drawing.Size(197, 39);
+            this.btn_Dashboard.TabIndex = 6;
+            this.btn_Dashboard.Text = "DASHBOARD";
+            this.btn_Dashboard.UseVisualStyleBackColor = false;
+            // 
+            // btn_Logout
+            // 
+            this.btn_Logout.BackColor = System.Drawing.Color.SlateGray;
+            this.btn_Logout.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Logout.Location = new System.Drawing.Point(1029, 11);
+            this.btn_Logout.Name = "btn_Logout";
+            this.btn_Logout.Size = new System.Drawing.Size(95, 35);
+            this.btn_Logout.TabIndex = 2;
+            this.btn_Logout.Text = "LOGOUT";
+            this.btn_Logout.UseVisualStyleBackColor = false;
+            // 
+            // btn_Copy
+            // 
+            this.btn_Copy.BackColor = System.Drawing.Color.SteelBlue;
+            this.btn_Copy.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Copy.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Copy.Location = new System.Drawing.Point(11, 423);
+            this.btn_Copy.Name = "btn_Copy";
+            this.btn_Copy.Size = new System.Drawing.Size(197, 39);
+            this.btn_Copy.TabIndex = 10;
+            this.btn_Copy.Text = "COPY";
+            this.btn_Copy.UseVisualStyleBackColor = false;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 781);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Home";
+            this.Text = "Home";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_Borrower;
+        private System.Windows.Forms.Button btn_Author;
+        private System.Windows.Forms.Button btn_Book;
+        private System.Windows.Forms.Button btn_Dashboard;
+        private System.Windows.Forms.Button btn_Logout;
+        private System.Windows.Forms.Button btn_Copy;
+    }
+}
