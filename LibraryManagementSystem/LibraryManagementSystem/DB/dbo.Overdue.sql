bf5768c81be13961c72a080c3a2d7530ae788ae6
Prabhashi Hettiarachchi <63660305+Pubzzz@users.noreply.github.com>
2020-12-30 21:13:12 +0530

    Update Database

---
+++ LibraryManagementSystem/LibraryManagementSystem/DB/dbo.Overdue.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Overdue] (
+    [LoanID]     INT           NOT NULL,
+    [BorrowerID] NVARCHAR (50) NULL,
+    [NoOfDays]   INT           NULL,
+    [PaymentDue] INT           NULL,
+    PRIMARY KEY CLUSTERED ([LoanID] ASC)
+);
+
---
+++ LibraryManagementSystem/LibraryManagementSystem/DB/dbo.Payment.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Payment] (
+    [LoanID]     INT  NOT NULL,
+    [BorrowerID] INT  NOT NULL,
+    [Payment]    INT  NOT NULL,
+    [Date]       DATE NOT NULL,
+    PRIMARY KEY CLUSTERED ([LoanID] ASC)
+);
+
