1c269e5d2096be6caa73040a03390b76fed3939b
Prabhashi Hettiarachchi <63660305+Pubzzz@users.noreply.github.com>
2021-01-01 16:40:34 +0530

    update

---
+++ LibraryManagementSystem/LibraryManagementSystem/DB/SP/spUpdate/dbo.spUpdateAuthors.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE spUpdateAuthors
+@authorId INT,
+@name NVARCHAR(50),
+@contact NCHAR(10),
+@email NVARCHAR(50)
+
+AS
+BEGIN
+	SET NOCOUNT ON;
+	UPDATE dbo.Author 
+	SET
+	[AuthorID] = @authorId,
+	[Name] = @name,
+	[Contact] = @contact,
+	[Email] = @email
+	WHERE [AuthorID] = @authorId
+END
